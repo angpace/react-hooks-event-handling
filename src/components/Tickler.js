586547f2ec1711d7eb15851aaca0ec3231ebafb3
Angela Pace <angpace13@gmail.com>
2022-10-13 11:12:13 -0400

    commit tree eacb2cdb0f28ade039322dbd470fd83f00cf4a8b

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
+  function tickle(event) {
+    console.log("Ive been Tickled");
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
 
 export default Tickler;
